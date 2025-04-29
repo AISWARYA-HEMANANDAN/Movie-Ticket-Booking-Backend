07652d1ec9ed28818c38dc26b68a9e8fee60688f
Aiswarya K T <kt.aiswarya@gmail.com>
2025-04-29 22:34:36 +0530

    db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
 const dbConnection = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("Db connection successfull");
-    } catch (error) {
-        console.log(error);
-    }
-}
-module.exports = dbConnection
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, 
+    });
+
+    console.log("MongoDB connected successfully.");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); 
+  }
+};
+
+module.exports = dbConnection;
